57ca5d44fc100f470872e85fd64f3e1e29dc0a82
Popyan <drobanton@gmail.com>
2017-03-26 17:40:05 +0300

    shmatrix

--- homeworks/matrix_multiplier/lib/Local/MatrixMultiplier.pm
+++ homeworks/matrix_multiplier/lib/Local/MatrixMultiplier.pm
@@ -6,7 +6,7 @@ use warnings;
 sub mult {
     my ($mat_a, $mat_b, $max_child) = @_;
     my $res = [];
-	my $param = @{$mat_a};
+	my $param = scalar @{$mat_a};
 	die if $param == 0;
 	for my $element (@{$mat_a}) {
 		die if $param != @{$element};	
@@ -15,20 +15,48 @@ sub mult {
 		die if $param != @{$element};	
 	}
 
+	my ($r, $w);
+	pipe ($r, $w);
+	my $last_row = (@{$mat_a}-1);
+	my $start = 0;
+	my $end = int($last_row/2);
 	my $pid = fork();
-	my $my_space = (@{$mat_a}-1)/2;
-	my $start = $pid * $my_space;
-	my $end = $start + $my_space;	
-
+	if ($pid) {#parent
+		close $w;
+	}else {#child
+		$start = int($last_row/2) + 1;
+		$end = $last_row;
+		close $r;
+	}
+	
 	for my $i ($start..$end) {
-		for my $j ($start..$end) {
+		for my $j (0..$last_row) {
 			$res->[$i][$j] = 0;	
-			for my $k (0..$param) {
+			for my $k (0..$last_row) {
 				$res->[$i][$j] += $mat_a->[$i][$k] * $mat_b->[$k][$j];	
 			}	
 		}	
 	}
-    return $res;
+
+	if ($pid) {#parent
+		waitpid($pid,0);
+		my ($i,$j) = ($end, 0);		
+		while (<$r>) {
+			$i++ if $j == 0;
+			$res->[$i][$j] = 0 + $_;
+			$j = ($j+1) % ($last_row+1);
+		}
+		close $r;
+	}else {#child
+		for my $i ($start..$end) {
+			for my $j (0..$last_row) {
+				print $w "$res->[$i][$j]\n"; 
+			}
+		}
+		close $w;
+	}
+	die unless $pid;
+    return $res if $pid;
 }
 
 1;
