21c0f401ea700cd80ff47b86fbb47a4cec8fc66d
Popyan <drobanton@gmail.com>
2017-04-09 14:48:51 +0300

    multiplier is ready

--- homeworks/matrix_multiplier/lib/Local/MatrixMultiplier.pm
+++ homeworks/matrix_multiplier/lib/Local/MatrixMultiplier.pm
@@ -2,10 +2,13 @@ package Local::MatrixMultiplier;
 
 use strict;
 use warnings;
+use POSIX qw(sys_wait_h);
+
 
 sub mult {
-    my ($mat_a, $mat_b, $max_child) = @_;
-    my $res = [];
+    my ($mat_a, $mat_b, $max_child) = @_; 
+	my $res = [];
+	#check matrix	
 	my $param = scalar @{$mat_a};
 	die if $param == 0;
 	for my $element (@{$mat_a}) {
@@ -15,48 +18,58 @@ sub mult {
 		die if $param != @{$element};	
 	}
 
-	my ($r, $w);
-	pipe ($r, $w);
-	my $last_row = (@{$mat_a}-1);
-	my $start = 0;
-	my $end = int($last_row/2);
-	my $pid = fork();
-	if ($pid) {#parent
-		close $w;
-	}else {#child
-		$start = int($last_row/2) + 1;
-		$end = $last_row;
-		close $r;
-	}
-	
-	for my $i ($start..$end) {
-		for my $j (0..$last_row) {
-			$res->[$i][$j] = 0;	
-			for my $k (0..$last_row) {
-				$res->[$i][$j] += $mat_a->[$i][$k] * $mat_b->[$k][$j];	
-			}	
-		}	
-	}
-
-	if ($pid) {#parent
-		waitpid($pid,0);
-		my ($i,$j) = ($end, 0);		
-		while (<$r>) {
-			$i++ if $j == 0;
-			$res->[$i][$j] = 0 + $_;
-			$j = ($j+1) % ($last_row+1);
-		}
-		close $r;
-	}else {#child
-		for my $i ($start..$end) {
-			for my $j (0..$last_row) {
-				print $w "$res->[$i][$j]\n"; 
+	my %childs;
+	my $start;
+	my $end;
+	my $last_row = @{$mat_a}-1;
+	my $interval = int(@{$mat_a}/$max_child);	
+	for my $i(0..$max_child-1){
+		my ($r, $w);
+		pipe ($r, $w);
+		my $pid = fork();
+		if($pid) {#parent
+			close $w;
+			$childs{$pid} = {iter => $i, reader => $r};
+			#reading		
+			waitpid($pid, WNOHANG);
+			my ($s,$j) = ($childs{$pid}{iter}*$interval - 1, 0);		
+			my $buff = $childs{$pid}{reader};			
+			while (<$buff>) {
+				$s++ if $j == 0;
+				$res->[$s][$j] = 0 + $_;
+				$j = ($j + 1)%($last_row + 1);
 			}
+			close $childs{$pid}{reader};
+		}else {#child
+			close $r;			
+			#definition of frames of the task for each slave, master does obviously nothing
+			if ($i == $max_child-1) {
+				$start = $interval*$i;
+				$end = $last_row;
+			}else {
+				$start = $i*$interval;
+				$end = ($i + 1)*$interval - 1;
+			}
+			#calculation
+			for my $s ($start..$end) {
+				for my $j (0..$last_row) {
+					$res->[$s][$j] = 0;	
+					for my $k (0..$last_row) {
+						$res->[$s][$j] += $mat_a->[$s][$k] * $mat_b->[$k][$j];	
+					}	
+				}	
+			}		
+			#writing
+			for my $j ($start..$end) {
+				for my $k (0..$last_row) {
+					print $w "$res->[$j][$k]\n";
+				}
+			}
+			close $w;	
+			exit;		
 		}
-		close $w;
 	}
-	die unless $pid;
-    return $res if $pid;
+    return $res;
 }
 
 1;
